a80b76be09a1fed3de06e3de736effab609ed8fc
lidiaamaral <amaralidia@gmail.com>
2023-11-02 15:36:06 -0300

    fix:initiol commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const convertButton = document.querySelector(".convert-button")
+
+function convert() {
+
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    console.log(inputCurrencyValue)
+
+  
+}
+
+convertButton.addEventListener("click", convert)
